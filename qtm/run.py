236847b78606b1fd753bfaa3401e4905c232312c
minhtriet <minhtriet09@gmail.com>
2024-07-15 19:51:26 +0100

    wip refactor

--- qtm/run.py
+++ qtm/run.py
@@ -1,6 +1,16 @@
 from omegaconf import OmegaConf
+import os
+import json
+
+
+def _load_json(*dir):
+    json_file = os.path.join(*dir)
+    with open(json_file) as f:
+        js = json.load(f)
+    return js
 
 OmegaConf.register_new_resolver("concat", lambda x, y: x+y)
+OmegaConf.register_new_resolver("load_json", _load_json)
 _ = OmegaConf.load("chem_config.yaml")
 OmegaConf.resolve(_)
 print(_)
