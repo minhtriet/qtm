665f1f5650d4d47cc3cbf843f1811d613b13f5b9
minhtriet <minhtriet09@gmail.com>
2024-07-24 23:15:54 +0100

    set worker count

--- qtm/run.py
+++ qtm/run.py
@@ -77,9 +77,8 @@ if __name__ == "__main__":
 
 
     bo = BayesianOptimizer(bound_config, reaction)
-    scenario = Scenario(bo.cs, deterministic=True, n_trials=4, n_workers=1)
-
-    # scenario = Scenario(self.cs, deterministic=True, n_trials=4, n_workers=os.cpu_count()-4)
+    # scenario = Scenario(bo.cs, deterministic=True, n_trials=4, n_workers=1)
+    scenario = Scenario(bo.cs, deterministic=True, n_trials=4, n_workers=max(1,os.cpu_count()-4))
     smac = BlackBoxFacade(scenario, bo.black_box, overwrite=True, dask_client=None)
     incumbent = smac.optimize()
     print(incumbent)
